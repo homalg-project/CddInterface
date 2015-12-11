3733a17b735acbfbda4c22fb289f94a09b034735
kamalsaleh <ksaleh87@yahoo.com>
2015-12-11 19:46:08 +0100

    combined all c files in one file, since location of some files (cdd.c) may cause a problem by installing the package

--- src/CddInterface.c
+++ src/CddInterface.c
@@ -3,35 +3,317 @@
  */
 
 #include "src/compiled.h"          /* GAP headers */
-#include "pkg/CddInterface/src/nothing.c"
-#include "pkg/CddInterface/src/cdd.c"
+#include "setoper.h"
+#include "cdd.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
 
 /**********************************************************
 *
 *    Auxiliary functions to be used inside C 
 * 
 * ********************************************************/
-static  int* iwjeiwjojdowije( int n )
+
+char* A2String(int *n, int size)
 {
- static  int r[50000];
-  int i;
-  
-  for( i=0;i<n;i++)
-    r[i]=i*2;
-  return r;
+        static char s1[dd_linelenmax],s[dd_linelenmax]= "";
+        int i;
+        strcat(s, " ");
+        for( i=0;i<size;i++)
+           {
+           sprintf(s1, "%d", *(n+i) );
+           strcat(s,s1);
+           strcat(s, " ");
+           } 
+        return s;
 }
 
-static int * kemo( int m )
+
+dd_MatrixPtr ddG_CanonicalizeMatrix( dd_MatrixPtr M)
 {
-  static int a[50000];
-  int i;
+  dd_MatrixPtr N=M;
+  dd_rowset impl_linset, redset;
+  dd_rowindex newpos;
+  dd_ErrorType err;
+  dd_MatrixCanonicalize(&N, &impl_linset, &redset, &newpos, &err);
   
-  for( i=0;i<m;i++)
-    a[i]=i*i;
-  
-  return a;
+  return N;
 }
 
+
+dd_MatrixPtr ddG_PolyInput2Matrix (int k_rep, int k_numtype,int k_linearity, dd_rowrange k_rowrange, 
+                         dd_colrange k_colrange,char k_linearity_array[50000],char k_matrix[50000],
+                         int k_LPobject, char k_rowvec[50000])
+                                  
+{
+char numbtype[50000], k_value[50000], k_matrixx[50000],k_linearity_arrayx[50000], k_rowvecx[50000];
+dd_MatrixPtr M=NULL;
+ dd_rowrange m_input,i;
+ dd_colrange d_input,j;
+ dd_RepresentationType rep;
+ dd_boolean found=dd_FALSE, newformat=dd_FALSE, successful=dd_FALSE, linearity=dd_FALSE;
+ dd_NumberType NT;
+ dd_LPObjectiveType ob;
+ mytype rational_value;
+ static mytype value;
+ char * pch;
+ div_t z;
+ int u;
+ 
+ strcpy( k_matrixx, k_matrix);
+ strcpy( k_linearity_arrayx, k_linearity_array );
+ strcpy( k_rowvecx, k_rowvec );
+ 
+// // creating the matrix with these two dimesnions
+   M=dd_CreateMatrix(k_rowrange, k_colrange);
+//   
+// 
+ // controling if the given representation is H or V.
+   if( k_rep == 2 ) {
+       rep=dd_Generator; newformat=dd_TRUE;
+     } else if (k_rep == 1)
+     {
+       rep=dd_Inequality; newformat=dd_TRUE;
+     } else 
+     {
+       rep=dd_Unspecified; newformat=dd_TRUE;
+     }
+ 
+   M->representation=rep;
+//   
+// controling the numbertype in the matrix
+// this may seem silly, but it should be so for compatiblity with cdd.
+   if (k_numtype==3) {  
+   strcpy(numbtype, "integer");
+   } else if (k_numtype==2) { 
+     strcpy(numbtype, "rational");
+   } else if (k_numtype==1) {
+     strcpy(numbtype, "real");}
+     else { strcpy(numbtype, "unspecified");}
+ 
+   NT=dd_GetNumberType(numbtype);
+//   
+   M->numbtype=NT;
+//   
+//  controling the linearity of the given polygon.
+   if (k_linearity==1) { 
+     linearity= dd_TRUE;
+     dd_SetLinearity(M,k_linearity_arrayx);
+   }
+//  
+// // filling the matrix with elements scanned from the string k_matrix
+    u=0;
+    pch = strtok (k_matrixx," ,.{}][");
+    while(pch != NULL) {
+      
+           strcpy( k_value, pch);
+           dd_init( rational_value );
+           dd_sread_rational_value (k_value, rational_value);
+           dd_set(value,rational_value);
+           dd_clear(rational_value);
+           z= div(u, k_colrange );
+           dd_set(M->matrix[z.quot][z.rem],value);
+           u= u+1;
+           pch = strtok (NULL, " ,.{}][");
+       }
+  successful=dd_TRUE;
+  
+  if (k_LPobject==0 ) { M->objective=dd_LPnone; } 
+  else if ( k_LPobject == 1 ) { M->objective= dd_LPmax; } 
+  else { M->objective = dd_LPmin; }
+  
+  
+  if (k_LPobject==1 || k_LPobject==2 ) 
+  {
+  pch =strtok( k_rowvecx, " ,.{}][");
+  for(u=0;u< M-> colsize; u++)
+  {
+    strcpy( k_value, pch );
+    dd_init( rational_value );
+    dd_sread_rational_value(k_value, rational_value );
+    dd_set( value, rational_value );
+    dd_clear( rational_value );
+    dd_set( M->rowvec[u], value );
+    pch= strtok( NULL, " ,.{}][" );
+  }  
+  }
+
+  
+  return M;
+}
+
+
+
+
+
+dd_LPSolutionPtr ddG_LPSolutionPtr( dd_MatrixPtr M )
+{
+  static dd_ErrorType err=dd_NoError;
+  static dd_LPPtr lp;
+  static dd_LPSolverType solver=dd_DualSimplex;
+  static dd_LPSolutionPtr lps;
+  
+  lp=dd_Matrix2LP(M, &err);
+  
+  dd_LPSolve(lp, solver, &err);
+  
+  lps= dd_CopyLPSolution( lp );
+  
+  return  lps;
+  
+}
+
+
+dd_rowrange ddG_RowSize( dd_MatrixPtr M)
+{
+  return M-> rowsize;
+}
+
+dd_colrange ddG_ColSize( dd_MatrixPtr M)
+{
+  return M-> colsize;
+}
+
+dd_rowset ddG_RowSet( dd_MatrixPtr M )
+{
+  return M->linset;
+}
+
+
+
+int ddG_LinearitySize( dd_MatrixPtr M )
+{
+  dd_rowrange r;
+  dd_rowset s;
+  int i,u;
+  
+  r= ddG_RowSize( M );
+  s= ddG_RowSet( M );
+  
+  u=0;
+  for(i=1;i<=r;i++)
+    if (set_member(i, s) ) {
+      u=u+1;
+    }
+    
+    return u;
+}
+  
+
+int * ddG_LinearityPtr(dd_MatrixPtr M )
+{
+  dd_rowrange r;
+  dd_rowset s;
+  int i,u;
+  static int lin_array[50000];
+  
+  r= ddG_RowSize( M );
+  s= ddG_RowSet( M );
+  
+  u=0;
+  for(i=1;i<=r;i++)
+    if (set_member(i, s) ) {
+      lin_array[u]=i;
+      u=u+1;
+    }
+    
+    return lin_array;
+}
+
+
+
+long int * ddG_RowVecPtr( dd_MatrixPtr M )
+{
+  static long int RowVec_array[50000];
+  static mpz_t u,v;
+  int i,z1,z2;
+  dd_Arow row_vector;
+  mpz_init(u);mpz_init( v);
+  
+  row_vector=M->rowvec;
+  
+  for(i=0;i< M->colsize;i++)
+  {
+       mpq_get_num(u, *(row_vector+i) );
+       mpq_get_den(v, *(row_vector+i) );
+       z1=mpz_get_si( u );
+       z2=mpz_get_si( v );
+       RowVec_array[2*i]=z1;
+       RowVec_array[2*i+1]=z2;
+  }
+  mpz_clear(u);mpz_clear(v);
+  
+  return RowVec_array;
+} 
+
+
+
+
+
+
+
+long int * ddG_AmatrixPtr( dd_MatrixPtr M )
+{
+  static long int Amatrix_array[50000];
+  dd_rowrange r;
+  dd_colrange s;
+  dd_Amatrix Ma;
+  int r1,s1,k,i,j,z1,z2;
+  mpz_t u,v;
+  
+  mpz_init(u);mpz_init(v);
+  
+  r= ddG_RowSize( M );
+  s= ddG_ColSize( M );
+  Ma = M->matrix;
+  
+   for(i=0;i<r;i++)
+     for(j=0;j<s;j++)
+     {
+       mpq_get_num(u,  *(*(Ma+i)+j)  );
+       mpq_get_den(v,  *(*(Ma+i)+j)  );
+         z1=mpz_get_si( u );
+         z2=mpz_get_si( v );
+        Amatrix_array[2*(i*s+j)]=z1;
+        Amatrix_array[2*(i*s+j)+1]=z2;
+     }
+ return Amatrix_array;
+  
+}
+
+
+
+
+
+
+int ddG_RepresentationType( dd_MatrixPtr M )
+{ 
+  return M->representation;
+}
+
+int ddG_NumberType( dd_MatrixPtr M )
+{ 
+  return M->numbtype; 
+}
+
+dd_Amatrix ddG_Matrix( dd_MatrixPtr M )
+{ 
+  return M-> matrix;
+}
+
+
+
+int ddG_IsOptimal( dd_MatrixPtr M )
+{
+  static dd_LPSolutionPtr lps;
+  lps = ddG_LPSolutionPtr( M );
+  
+  if (lps->LPS==dd_Optimal) return 1; 
+    else return 0;
+}
 
 static char * RATPLIST_STR( Obj list )
 {
@@ -129,10 +411,7 @@ static Obj MatrixPtrToGapObj( dd_MatrixPtr M )
   current= INTOBJ_INT( ddG_ColSize( M ) );
   SET_ELM_PLIST(result, 5, current  );
   CHANGED_BAG( result );
-  
-//   ErrorMayQuit( A2String( ddG_LinearityPtr( M ), 2 ), 0, 0);
-//   return NULL;
-  
+    
    size_t i1, size1;
    int i, size;
    int * lin;
@@ -170,12 +449,6 @@ static Obj MatrixPtrToGapObj( dd_MatrixPtr M )
    
   SET_ELM_PLIST(result, 7, current  );
   CHANGED_BAG( result );
-  
-   
-  
-//   current= NEW_PLIST(T_PLIST_CYC, size1 );
-//   SET_LEN_PLIST( current, size1 );
-
    
   
   
@@ -183,9 +456,6 @@ static Obj MatrixPtrToGapObj( dd_MatrixPtr M )
 }
 
 static dd_MatrixPtr GapInputToMatrixPtr( Obj input )
-
-// Obj rep, Obj numtype, Obj linearity, Obj rowrange, Obj colrange, Obj linearity_array, Obj matrix, Obj LPobject, Obj rowvec )
-
 {
   
   int k_rep,k_numtype,k_linearity, k_rowrange, k_colrange, k_LPobject;
@@ -207,12 +477,6 @@ static dd_MatrixPtr GapInputToMatrixPtr( Obj input )
     
   strcpy( k_linearity_array, PLIST_STR( ELM_PLIST( input , 6 ) ) );
   strcpy( k_rowvec,          RATPLIST_STR( ELM_PLIST( input , 9 ) ) );
-    
-//     ErrorMayQuit( k_matrix, 0,0 );
-//     return NULL;
-//    return ddG_PolyInput2Matrix( k_rep,k_numtype,k_linearity, k_rowrange, k_colrange, 
-//    k_linearity_array, k_matrix, k_LPobject , k_rowvec );
-  
 
   return ddG_PolyInput2Matrix( k_rep , k_numtype, k_linearity, k_rowrange, k_colrange, k_linearity_array, k_matrix , k_LPobject, k_rowvec  );
 }       
@@ -394,87 +658,6 @@ static Obj CddInterface_LpSolution( Obj self, Obj main )
    
 }
   
-
-
-
-
-Obj take_poly_and_give_it_back( Obj self, Obj list )
-{
-  dd_MatrixPtr M;
-  
-  M= GapInputToMatrixPtr( list );
-  
-   return  MatrixPtrToGapObj( M ) ;
-  
-//   return INTOBJ_INT( 2 );
-}
-
-Obj take_it_and_give_it_back( Obj self, Obj list )
-{
-  return CINTLISTPtr_TOGAPPLIST( GAPPLIST_TOINTPtr(list), LEN_PLIST( list)  );
-}
-
-
-
-Obj TestCommandWithParams(Obj self, Obj param, Obj param2)
-{
-    /* simply return the first parameter */
-return param; 
-  
-}
- 
-Obj TestCommand_max( Obj self, Obj param1, Obj param2 )
-{
-  int a,b;
-  a= INT_INTOBJ( param1 );
-  b= INT_INTOBJ( param2 );
-  
-  return INTOBJ_INT( TestKemoSum( a,b ) );
-//   if (a>b) return param1; else return param2;
-}
-
-Obj testkamalove( Obj self )
-{
-  
-  Obj L;
-  size_t n=0;
-  
-  L = NEW_PLIST((n > 0) ? T_PLIST_CYC : T_PLIST, n);
-  SET_LEN_PLIST(L, n);
-  SET_ELM_PLIST( L, n+1, INTOBJ_INT( 4 ) );
-//   L:= NEW_PLIST(T_PLIST_CYC, 1 );
-//   SET_LEN_PLIST( L, 1 );
-  
-//    int array[50000]={5,8,9,3,45,3};
-//   size_t i=6;
-  
-//   static  int * r;
-//   r= iwjeiwjojdowije(6);
- /* 
-  
-  for( i=0;i++;i<6 )
-  {
-     array[i]= 4;
-  }
-  */
-//   return CLIST_TOGAPPLIST( array, i );
-// return CINTLISTPtr_TOGAPPLIST( iwjeiwjojdowije( 3 ), 3 );
-return L;
-}
-
-
-  
-/*   
-static Obj MpqToGap( mpq_t x )
-{
-  mpz_t num, den;
-  
-  mpq_get_num(num, x);
-  mpq_get_num(den, x);
-  
-  return QUO( MpzToGAP( num ), MpzToGAP( den ) );
-}*/
-
 /******************************************************************/
 
 typedef Obj (* GVarFunc)(/*arguments*/);
@@ -488,15 +671,15 @@ typedef Obj (* GVarFunc)(/*arguments*/);
 // Table of functions to export
 static StructGVarFunc GVarFuncs [] = {
   
-    GVAR_FUNC_TABLE_ENTRY("CddInterface.c", TestCommandWithParams, 2, "param, param2"),
-    GVAR_FUNC_TABLE_ENTRY("CddInterface.c", TestCommand_max, 2, "param1, param2"),
-    GVAR_FUNC_TABLE_ENTRY("CddInterface.c", testkamalove, 0, ""),
-    GVAR_FUNC_TABLE_ENTRY("CddInterface.c", take_it_and_give_it_back, 1, "list"),
+//     GVAR_FUNC_TABLE_ENTRY("CddInterface.c", TestCommandWithParams, 2, "param, param2"),
+//     GVAR_FUNC_TABLE_ENTRY("CddInterface.c", TestCommand_max, 2, "param1, param2"),
+//     GVAR_FUNC_TABLE_ENTRY("CddInterface.c", testkamalove, 0, ""),
+//     GVAR_FUNC_TABLE_ENTRY("CddInterface.c", take_it_and_give_it_back, 1, "list"),
     GVAR_FUNC_TABLE_ENTRY("CddInterface.c", CddInterface_Canonicalize, 1, "main"),
     GVAR_FUNC_TABLE_ENTRY("CddInterface.c", CddInterface_Compute_H_rep, 1, "main"),
     GVAR_FUNC_TABLE_ENTRY("CddInterface.c", CddInterface_Compute_V_rep, 1, "main"),
     GVAR_FUNC_TABLE_ENTRY("CddInterface.c", CddInterface_LpSolution, 1, "main"),
-    GVAR_FUNC_TABLE_ENTRY("CddInterface.c", take_poly_and_give_it_back, 1, "list"),
+//     GVAR_FUNC_TABLE_ENTRY("CddInterface.c", take_poly_and_give_it_back, 1, "list"),
     
     { 0 } /* Finish with an empty entry */
 
